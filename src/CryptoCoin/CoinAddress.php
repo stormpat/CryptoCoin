fdc5a10f53bba8d28500cdce7f07e456d81a555c
Patrik Storm <storm.patrik@gmail.com>
2014-01-10 14:06:06 +0200

    removed all instances of mt_rand for more security

--- src/CryptoCoin/CoinAddress.php
+++ src/CryptoCoin/CoinAddress.php
@@ -354,12 +354,32 @@ class CoinAddress
     public static function create_key_pair()
     {
 
-        $privBin = '';
-        for ($i = 0; $i < 32; $i++)
+        do
         {
-            $privBin .= chr(mt_rand(0, $i ? 0xff : 0xfe));
-        }
-        $point = Point::mul(BcmathUtils::bin2bc("\x00" . $privBin), self::$secp256k1_G);
+             $privBin = openssl_random_pseudo_bytes(32, $crypto_strong);
+
+             if (!$crypto_strong)
+             {
+                 throw new RuntimeException('crypto-strong RNG not available!');
+             }
+
+             $secretMultiplier = BcmathUtils::bin2bc("\x00" . $privBin);
+
+             if (USE_EXT == 'GMP')
+             {
+                 $lessThanOrderG = gmp_cmp(self::$secp256k1_G->getOrder(), $secretMultiplier) > 0;
+             }
+             else if (USE_EXT == 'BCMATH')
+             {
+                 $lessThanOrderG = bccomp(self::$secp256k1_G->getOrder(), $secretMultiplier) > 0;
+             }
+             else
+             {
+                 throw new RuntimeException('USE_EXT not configured correctly');
+             }
+        } while (!$lessThanOrderG);
+
+       $point = Point::mul($secretMultiplier, self::$secp256k1_G);
 
         $pubBinStr = "\x04" . str_pad(BcmathUtils::bc2bin($point->getX()), 32, "\x00", STR_PAD_LEFT) . str_pad(BcmathUtils::bc2bin($point->getY()), 32, "\x00", STR_PAD_LEFT);
 
